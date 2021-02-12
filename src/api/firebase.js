852c86ed51b51a5b79731aff8048364f164b44b8
Elenilson Monnteiro <33660823+eleMonteiro@users.noreply.github.com>
2021-02-12 18:12:22 -0300

    Update firebase.js

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -5,14 +5,14 @@ import 'firebase/database'
 import 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8OCqQJUAywRIgRHciWVyNOBuBvJ-fTeM",
-  authDomain: "classifiqui-70e14.firebaseapp.com",
-  databaseURL: "https://classifiqui-70e14-default-rtdb.firebaseio.com",
-  projectId: "classifiqui-70e14",
-  storageBucket: "classifiqui-70e14.appspot.com",
-  messagingSenderId: "272221561285",
-  appId: "1:272221561285:web:8d8b93cb66c12bbfca2bac",
-  measurementId: "G-T37CK4F1KZ"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 firebase.initializeApp(firebaseConfig)
